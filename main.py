acf1661cce0c7178508702fb1673e5edf6870e6b
Alaina Gossett <91034239+alainagossett@users.noreply.github.com>
2022-02-05 18:17:14 +0000

    adds control flow notes

--- main.py
+++ main.py
@@ -25,9 +25,26 @@ my_number = 15
 # reassigning variables
 my_number = 16
 # my_number++ won't work
-print('my_number: ', my_number)
+# print('my_number: ', my_number)
 
 # DATA TYPES
-print(type(42))
-print(type('hello world'))
-print(type(True))
+# print(type(42))
+# print(type('hello world'))
+# print(type(True))
+
+# CONTROL FLOW
+# print(1 == '1')
+# print(str(1) == '1')
+
+color = input('Enter "green", "yellow", "red": ').lower()
+print(f'The user entered {color}')
+
+if color == 'green':
+  print('Go!')
+elif color == 'yellow':
+  print('Slow Down!')
+elif color == 'red':
+  print('Stop!')
+else:
+  print('Bogus!')
+
