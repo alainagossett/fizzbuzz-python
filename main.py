fdcd503f91a0d78f9124a00bf1d7062c891f7ee4
Alaina Gossett <91034239+alainagossett@users.noreply.github.com>
2022-02-05 18:26:00 +0000

    adds traffic light function

--- main.py
+++ main.py
@@ -39,12 +39,14 @@ my_number = 16
 color = input('Enter "green", "yellow", "red": ').lower()
 print(f'The user entered {color}')
 
-if color == 'green':
-  print('Go!')
-elif color == 'yellow':
-  print('Slow Down!')
-elif color == 'red':
-  print('Stop!')
-else:
-  print('Bogus!')
+def traffic_light():
+  if color == 'green':
+    print('Go!')
+  elif color == 'yellow':
+    print('Slow Down!')
+  elif color == 'red':
+    print('Stop!')
+  else:
+    print('Bogus!')
 
+traffic_light()
